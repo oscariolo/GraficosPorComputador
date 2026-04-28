1b17798f0d3f31e5abfab22cba2973ff3fb6314a
oscariolo <oscar.orellana@ucuenca.edu.ec>
2026-04-28 16:51:42 -0500

    Base triangle

--- Programas/trianguloSierpinski/src/main.cpp
+++ Programas/trianguloSierpinski/src/main.cpp
@@ -16,6 +16,64 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     glfwSetWindowShouldClose(window, true);    
 }
 
+void baseTriangle(unsigned int &VBO, unsigned int &VAO)
+{
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,  // left  
+         0.5f, -0.5f, 0.0f,  // right 
+         0.0f,  0.5f, 0.0f   // top   
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+void generatePoint(unsigned int &VBO, unsigned int &VAO)
+{
+    float vertices[] = {
+        0.0f, 0.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}   
+
+
+
+
+void setUpShaders(unsigned int &shaders)
+{
+    unsigned int vertexShader;
+    shader_utils::sourceShader("vertex.shader",vertexShader,GL_VERTEX_SHADER);
+
+    unsigned int fragmentShader;
+    shader_utils::sourceShader("fragment.shader",fragmentShader,GL_FRAGMENT_SHADER);
+
+    shaders = glCreateProgram();
+    glAttachShader(shaders,vertexShader);
+    glAttachShader(shaders,fragmentShader);
+    glLinkProgram(shaders);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+}
+
+
 int main()
 {
     glfwInit();
@@ -43,14 +101,25 @@ int main()
 
     glViewport(0, 0, 800, 600);    
 
+    unsigned int triangleVBO,triangleVAO,shaders;
+    unsigned int pointVBO,pointVAO;
+    baseTriangle(triangleVBO,triangleVAO);
+    generatePoint(pointVBO,pointVAO);
+    setUpShaders(shaders); 
+
     //Render Loop
     while(!glfwWindowShouldClose(window))
     {
         //Rendering commands 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        //
+        glUseProgram(shaders);
+        glBindVertexArray(triangleVAO); 
+        glDrawArrays(GL_LINE_LOOP,0,3);
+
+        glBindVertexArray(pointVAO);
+        glDrawArrays(GL_POINTS,0,1);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
@@ -59,3 +128,4 @@ int main()
     ///
     return 0;
 }
+
--- Programas/trianguloSierpinski/src/utils/shader_utils.h
+++ Programas/trianguloSierpinski/src/utils/shader_utils.h
@@ -32,8 +32,21 @@ inline void checkShaderCompilation(unsigned int shader)
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
     if (!success) {
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 }
 
+inline void sourceShader(const char* name,unsigned int &shader, GLenum shaderType)
+{
+    const char* typeStr = (shaderType == GL_VERTEX_SHADER) ? "vertex" : "fragment";
+    //Si no da shaderSource se asume el mismo path base 
+    std::string shaderCode = shader_utils::loadShaderFromSource(std::filesystem::path(__FILE__).parent_path().parent_path(), typeStr, name);
+    const char* shaderSource = shaderCode.c_str();
+    shader = glCreateShader(shaderType);
+    glShaderSource(shader,1,&shaderSource,NULL);
+    glCompileShader(shader);
+    checkShaderCompilation(shader);
+}
+
 } // namespace shader_utils
+
